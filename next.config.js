c22bf1637328f11c3ea577c4aa1de0c611cbd5c5
Théo <theo.fabianomattei@gmail.com>
2023-11-05 13:55:28 +0100

    security: add remotePatterns to images in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+
+require("dotenv").config();
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: `${process.env.SUPABASE_REFERENCE_ID}.supabase.co`,
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
