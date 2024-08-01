61f85651673be2df45ba64db8f2d488750184996
Théo <theo.fabianomattei@gmail.com>
2024-08-01 17:13:49 +0200

    chore: update Stripe API version to "2024-06-20"

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import pkg from "../package.json";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-06-20",
   appInfo: {
     name: "Spotify Clone",
     version: pkg.version,
